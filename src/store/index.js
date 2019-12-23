ce57b490b754760a61e3adeaf1f44c30c9f6052b
Bartosz <bartoszurbanski.biznes@gmail.com>
2019-12-23 15:35:46 +0100

    made action for fetching movies

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-Vue.use(Vuex)
+Vue.use(Vuex, VueAxios, axios)
 
 export default new Vuex.Store({
   state: {
+    movies: []
   },
   mutations: {
+    setMovies: (state, payload) => {
+      state.movies = payload
+    }
   },
   actions: {
+    searchByTitle: (context, payload) => {
+      axios.get("http://www.omdbapi.com/?apikey=37207fe&s=" + payload).then(response => {
+        context.commit('setMovies', response.data.Search)
+      })
+    }
   },
   modules: {
   }
