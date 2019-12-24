a1dbcb06f66f76f31b8bc7e141438d770f71c992
Bartosz <bartoszurbanski.biznes@gmail.com>
2019-12-24 11:51:15 +0100

    made whole main component

--- src/components/TheMainContent.vue
+++ src/components/TheMainContent.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
-    <main class="page__main"></main>
+    <main class="page__main main">
+      <MovieCard
+        class="main__movie-card"
+        v-for="(movie, index) in movies"
+        :key="movie+index"
+        :movie="movie"
+      />
+    </main>
   </div>
 </template>
+<script>
+import MovieCard from "@/components/MovieCard.vue";
+export default {
+  computed: {
+    movies() {
+      return this.$store.state.movies;
+    }
+  },
+  components: {
+    MovieCard
+  }
+};
+</script>
+<style lang="scss">
+.main {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
