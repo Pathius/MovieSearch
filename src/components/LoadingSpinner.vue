117acacf993a1e74ba8c40e9c0322fce6810ca63
Bartosz <bartoszurbanski.biznes@gmail.com>
2019-12-28 15:41:53 +0100

    created component for lazy loading

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="spinner"></div>
+  </div>
+</template>
+<style lang="scss">
+.spinner {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 5px solid transparent;
+  border-top-color: white;
+  border-left-color: white;
+  border-bottom-color: white;
+  animation: loading infinite linear 2s;
+}
+@keyframes loading {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
