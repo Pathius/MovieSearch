3be3850aa554513289ea30ea8870fd0f8465bdfc
Bartosz <bartoszurbanski.biznes@gmail.com>
2019-12-28 15:41:39 +0100

    created action, mutation and state for actual movie

--- src/store/index.js
+++ src/store/index.js
@@ -8,17 +8,35 @@ Vue.use(Vuex, VueAxios, axios)
 export default new Vuex.Store({
   state: {
     movies: [],
+    actualMovie: {}
   },
   mutations: {
+    resetMovies: (state) => {
+      state.movies = []
+    },
+    resetActualMovie: (state) => {
+      state.actualMovie = {}
+    },
     setMovies: (state, payload) => {
       state.movies = payload
+    },
+    setActualMovie: (state, payload) => {
+      state.actualMovie = payload
     }
   },
   actions: {
     searchByTitle: (context, payload) => {
+      context.commit("resetMovies")
       axios.get("https://api.themoviedb.org/3/search/movie?api_key=4a2556ad0cbaf0c9369adc89e3aaf7de&query=" + payload)
         .then(response => {
           context.commit("setMovies", response.data.results)
+        })
+    },
+    findExactMovie: (context, payload) => {
+      context.commit("resetActualMovie")
+      axios.get(`https://api.themoviedb.org/3/movie/${payload}?api_key=4a2556ad0cbaf0c9369adc89e3aaf7de`)
+        .then(response => {
+          context.commit("setActualMovie", response.data)
         })
     }
   },
