ae9fe6337e443a228d3a9e747b38d757a837af48
Bartosz <bartoszurbanski.biznes@gmail.com>
2019-12-28 15:42:10 +0100

    created favourite movies view

---
+++ src/views/TheFavouriteMovies.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <section class>
+      <i class="fas fa-long-arrow-alt-left"></i>
+      <h2>Your favourite movies</h2>
+    </section>
+    <MovieCard class="main__movie-card" />
+  </div>
+</template>
+<script>
+import MovieCard from "@/components/MovieCard.vue";
+export default {
+  components: {
+    MovieCard
+  }
+};
+</script>
+<style lang="scss">
+</style>
