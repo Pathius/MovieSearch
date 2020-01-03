80c2e39894a5ecf5a39db00b699633a929303a78
Bartosz <bartoszurbanski.biznes@gmail.com>
2020-01-03 16:04:00 +0100

    created 2 major components

---
+++ src/views/ThePopularMovies.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <section class="upcoming">
+      <h2 class="upcoming__title">Popular movies</h2>
+      <MovieCard
+        class="upcoming__movie-card"
+        v-for="movie in movies"
+        :key="movie.id"
+        :movie="movie"
+      />
+    </section>
+  </div>
+</template>
+<script>
+import MovieCard from "@/components/MovieCard";
+export default {
+  computed: {
+    movies() {
+      return this.$store.state.popularMovies;
+    }
+  },
+  components: {
+    MovieCard
+  }
+};
+</script>
+<style scoped lang="scss">
+.upcoming {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  text-align: center;
+  padding-bottom: 60px;
+  &__title {
+    width: 100%;
+    margin-top: 0;
+    padding-top: 40px;
+    font-size: 32px;
+  }
+}
+</style>
